369fd3212d84bacf0ceeb93dc2b0b90a4c0d7b85
o.chikami <occhitta@gmail.com>
2024-07-15 04:52:24 +0900

    コメント修正とメソッドに「public」を追加

--- csharp/source/code/Stream/StringHeavyReader.cs
+++ csharp/source/code/Stream/StringHeavyReader.cs
@@ -3,7 +3,7 @@ using System.Threading;
 namespace Occhitta.Libraries.Stream;
 
 /// <summary>
-/// バイナリ読込処理インターフェースです。
+/// テキスト読込処理定義体です。
 /// </summary>
 public interface StringHeavyReader : IDisposable {
 	/// <summary>
@@ -11,7 +11,7 @@ public interface StringHeavyReader : IDisposable {
 	/// </summary>
 	/// <param name="cancel">取消処理</param>
 	/// <returns>後続情報</returns>
-	Task<int> Read(CancellationToken cancel = default);
+	public Task<int> Read(CancellationToken cancel = default);
 	/// <summary>
 	/// 後続情報を読込みます。
 	/// </summary>
@@ -20,12 +20,12 @@ public interface StringHeavyReader : IDisposable {
 	/// <param name="length">保存個数</param>
 	/// <param name="cancel">取消処理</param>
 	/// <returns>保存した個数を返却</returns>
-	Task<int> Read(char[] buffer, int offset, int length, CancellationToken cancel = default);
+	public Task<int> Read(char[] buffer, int offset, int length, CancellationToken cancel = default);
 	/// <summary>
 	/// 後続情報を読込みます。
 	/// </summary>
 	/// <param name="buffer">保存配列</param>
 	/// <param name="cancel">取消処理</param>
 	/// <returns>保存した個数を返却</returns>
-	Task<int> Read(char[] buffer, CancellationToken cancel = default) => Read(buffer, 0, buffer.Length, cancel);
+	public Task<int> Read(char[] buffer, CancellationToken cancel = default) => Read(buffer, 0, buffer.Length, cancel);
 }
--- csharp/source/code/Stream/StringLightReader.cs
+++ csharp/source/code/Stream/StringLightReader.cs
@@ -1,7 +1,7 @@
 namespace Occhitta.Libraries.Stream;
 
 /// <summary>
-/// バイナリ読込処理インターフェースです。
+/// テキスト読込処理定義体です。
 /// </summary>
 public interface StringLightReader : IDisposable {
 	/// <summary>
@@ -9,7 +9,7 @@ public interface StringLightReader : IDisposable {
 	/// <para>終端に達した場合、負数を返却します</para>
 	/// </summary>
 	/// <returns>後続情報</returns>
-	int Read();
+	public int Read();
 	/// <summary>
 	/// 後続情報を読込みます。
 	/// </summary>
@@ -17,11 +17,11 @@ public interface StringLightReader : IDisposable {
 	/// <param name="offset">開始位置</param>
 	/// <param name="length">保存個数</param>
 	/// <returns>保存した個数を返却</returns>
-	int Read(char[] buffer, int offset, int length);
+	public int Read(char[] buffer, int offset, int length);
 	/// <summary>
 	/// 後続情報を読込みます。
 	/// </summary>
 	/// <param name="buffer">保存配列</param>
 	/// <returns>保存した個数を返却</returns>
-	int Read(char[] buffer) => Read(buffer, 0, buffer.Length);
+	public int Read(char[] buffer) => Read(buffer, 0, buffer.Length);
 }
